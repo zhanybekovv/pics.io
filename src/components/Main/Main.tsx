de04f218484fdc1247e1b6a03a0c6bd8a71a4d6c
zhanybekovv <nartay.zhanybekov@nu.edu.kz>
2021-06-16 22:27:53 +0600

    set up env

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,14 @@
+import React, {FC} from 'react';
+import { Props } from './types';
+import './index.css'
+
+
+const Main: FC<Props> = (props: Props) => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Main;
---
+++ src/components/Widgets/Widgets.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+import { Props } from './types';
+import './index.css';
+
+const Widgets: FC<Props> = (props: Props) => {
+	return <div>Widgets</div>;
+};
+
+export default Widgets;
